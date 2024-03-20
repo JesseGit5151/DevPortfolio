2bfe533ccf5cb510bcadb44cc4184e5d5537a0c9
JesseGit5151 <jrodriguez5151yahoo.com>
2024-03-20 12:43:08 -0700

    connected mongodb and added env file

--- API/index.js
+++ API/index.js
@@ -1,6 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
+
+mongoose.connect(process.env.MONGOID, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 app.get('/', (req, res) => {
     res.send('Home')
